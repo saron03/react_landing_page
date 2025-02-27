19300041e8bec4d231496324e8935ab90dc35947
Saron <saronyitbe@gmail.com>
2025-02-27 22:32:39 +0300

    add files

---
+++ landing-page/src/App.jsx
@@ -0,0 +1,8 @@
+import './components/header.jsx';
+import Header from './components/header.jsx';
+
+export default function App() {
+  return (
+    <Header/>
+  )
+}
---
+++ landing-page/src/components/header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <div>
+            <p>Coucher</p>
+            <ul>
+                <li>SERVICES</li>
+                <li>ABOUT US</li>
+                <li>CASES</li>
+            </ul>
+            <p>SIGN UP</p>
+        </div>
+    )
+}
